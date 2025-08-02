cc2425f7ca3c5e3df0bf3de1c20395e0c10899ea
Rafid Tahmid <rafidtahmid@Rafids-MacBook-Air.local>
2025-08-02 15:10:17 +0930

    added

--- Referee.cpp
+++ Referee.cpp
@@ -1,1 +1,14 @@
 #include "Referee.h"
+
+Player * Referee::refGame(Player * player1, Player * player2) {
+    char move1 = player1->makeMove();
+    char move2 = player2->makeMove();
+
+    if (move1 == move2) return nullptr;
+
+    if ((move1 == 'R' && move2 == 'S') ||
+        (move1 == 'P' && move2 == 'R') ||
+        (move1 == 'S' && move2 == 'P')) return player1;
+    return player2;
+}
+Referee::Referee() {}
---
+++ main.cpp
@@ -0,0 +1,21 @@
+
+#include "Human.h"
+#include "Computer.h"
+#include "Referee.h"
+#include "Player.h"
+#include <iostream>
+int main() {
+    Human human;
+    Computer computer;
+    Referee referee;
+    
+    Player *winner = referee.refGame(&human, &computer);
+    
+    if (winner == nullptr) {
+        std::cout <<"It's a Tie"<<std::endl;
+    } else {
+        std::cout << winner->getName()<< " Wins."<<std::endl;
+    }
+    return 0;
+
+}
